886ad468608b5f9bdaf8e94cb79e472f90db7aa8
tcphann <157539491+tcphann@users.noreply.github.com>
2024-04-02 22:06:53 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ function init(){
   function showMeText(){
     var textbox = document.getElementById('entryinput');
     document.getElementById('textoutput').innerHTML = textbox.value;
-    alert("Daniel Krieglstein: " + textbox.value);
+    alert("Tammy Phan: " + textbox.value);
   }
 
   button.addEventListener('click', showMeText);
